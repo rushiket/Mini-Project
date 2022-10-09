4e9a1061d981b2666468f66e4ebe7ddb406babe6
rushiket <mrushiket@gmail.com>
2022-10-09 18:27:14 +0530

    Initial commit

---
+++ backend/base/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+# Create your views here.
+
+def getRoutes(request):
+    routes = [
+        '/api/home',
+
+    ]
+    return JsonResponse(routes, safe=False)
+
+
