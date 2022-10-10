974a88e8a8f966e8eee2a798aa257899a2c2f170
rushiket <mrushiket@gmail.com>
2022-10-10 15:18:59 +0530

    backend

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.getRoutes,name="routes"),
+    path('data/',views.getData,name="data"),
+    path('data/<int:pk>/',views.getID,name="id"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+from .data import df
 
+@api_view(['GET'])
 def getRoutes(request):
-    routes = [
-        '/api/home',
+    routes = ['/api/home']
+    return Response(routes)
 
-    ]
-    return JsonResponse(routes, safe=False)
+@api_view(['GET'])
+def getData(request):
+    return Response(df)
 
+@api_view(['GET'])
+def getID(request,pk):
+    id = None
+    for i in df:
+        if i['ID'] == pk:
+            id = i
+            break
+    return Response(id)
 
