91d115bcbfd733564d26049c1dcaaf5f38654a33
rushiket <mrushiket@gmail.com>
2022-10-12 00:00:51 +0530

    Fetching data from backend

--- backend/base/data.py
+++ backend/base/data.py
@@ -4,16 +4,16 @@ import pandas as pd
 #df = pd.read_csv("D:\Personal Folder\Github\Mini Project\backend\MW-NIFTY-50-10-Oct-2022.csv",encoding="latin-1")
 
 df = [
-  {"ID":1,"SYMBOL ": "NIFTY 50"},
-  {"SYMBOL ": "AXISBANK","ID":2},
-  {"SYMBOL ": "TCS","ID":3},
-  {"SYMBOL ": "EICHERMOT","ID":4},
-  {"SYMBOL ": "MARUTI","ID":5},
-  {"SYMBOL ": "POWERGRID","ID":6},
-  {"SYMBOL ": "HDFCLIFE","ID":7},
-  {"SYMBOL ": "SBIN","ID":8},
-  {"SYMBOL ": "ICICIBANK","ID":9},
-  {"SYMBOL ": "COALINDIA","ID":10},
-  {"SYMBOL ": "M&M","ID":11},
-  {"SYMBOL ": "NTPC","ID":12}
-]
+    {"ID":1,"SYMBOL": "NIFTY 50"},
+    {"SYMBOL": "AXISBANK","ID":2},
+    {"SYMBOL": "TCS","ID":3},
+    {"SYMBOL": "EICHERMOT","ID":4},
+    {"SYMBOL": "MARUTI","ID":5},
+    {"SYMBOL": "POWERGRID","ID":6},
+    {"SYMBOL": "HDFCLIFE","ID":7},
+    {"SYMBOL": "SBIN","ID":8},
+    {"SYMBOL": "ICICIBANK","ID":9},
+    {"SYMBOL": "COALINDIA","ID":10},
+    {"SYMBOL": "M&M","ID":11},
+    {"SYMBOL": "NTPC","ID":12}
+  ]
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Share from "../components/Share";
-import data from "../sharesData";
+
+import axios from "axios";
+
+//import data from "../sharesData";
 
 function HomeScreen() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+        const { tempData } = await axios.get('/api/data/')
+        console.log(tempData)
+        setData(tempData)
+    }
+    
+    fetchData()
+    
+  }, []);
+
   return (
     <div>
       <Row>
